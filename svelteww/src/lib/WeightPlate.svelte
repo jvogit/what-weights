d45e00d6c5074cded1593fe628d26eb9469e7fa7
jvogit <justinxvopro@gmail.com>
2024-02-18 03:49:14 -0800

    barbell weights

---
+++ svelteww/src/lib/WeightPlate.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let weight: number;
+</script>
+
+<div class="weight-plate">
+    {weight}
+</div>
+
+<style>
+    .weight-plate {
+        width: 25px;
+        height: 225px;
+        background-color: #aa0000;
+    }
+</style>
---
+++ svelteww/src/routes/weight/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import WeightPlate from "$lib/WeightPlate.svelte";
+</script>
+
+<div class="container">
+    <div class="barbell-collar" />
+    <WeightPlate weight={1} />
+    <WeightPlate weight={2} />
+    <div class="barbell-sleeve" />
+</div>
+
+<style>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        gap: 0.2rem;
+    }
+
+    .barbell-sleeve {
+        flex-grow: 1;
+        height: 1.2rem;
+        background-color: #000000;
+    }
+
+    .barbell-collar {
+        width: 1rem;
+        height: 2.5rem;
+        background-color: #000000;
+    }
+</style>
